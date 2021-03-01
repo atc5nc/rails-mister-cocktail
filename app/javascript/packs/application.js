d5af77b701063d6d7cffa62e060841abc1a038b0
Alston Cobb <atc5nc@virginia.edu>
2021-02-28 21:12:02 -0300

    Updated setup

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,16 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+// require("@rails/ujs").start()
+// require("turbolinks").start()
+// require("@rails/activestorage").start()
+// require("channels")
+// import 'bootstrap';
+
 import 'bootstrap';
+import improveDropdown from '../components/select';
+
+improveDropdown();
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
